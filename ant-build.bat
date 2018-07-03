b5fa7a28bd45f13b0ec1484eed051bbcfece4db0
feilong <venusdrogon@163.com>
2018-07-03 12:04:08 +0800

    init

---
+++ ant-build.bat
@@ -0,0 +1,13 @@
+::设置命令窗口大小
+mode con cols=160 lines=2800
+
+@echo off
+rem 当前bat的作用
+echo 
+echo create by 金鑫
+
+title cleanAll
+
+call ant 
+
+pause
---
+++ ant-build.sh
@@ -0,0 +1,8 @@
+echo
+
+buildfile="/Users/feilong/work/clean-maven-and-eclipse/build.xml"
+
+echo ant -buildfile ${buildfile}
+echo
+
+ant -buildfile ${buildfile}
